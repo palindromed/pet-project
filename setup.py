9dd16ce08e02020f4eb21c5d9c8eb4e3d458124d
Kent Ross <root.main@gmail.com>
2016-03-15 15:36:36 -0700

    added pyramid scaffold

--- setup.py
+++ setup.py
@@ -1,17 +1,58 @@
 # coding=utf-8
-from setuptools import setup
+import os
 
-setup(
-    name="learning-journal",
-    description="Codefellows 401 Learning journal project",
-    version=0.1,
-    author="Kent Ross, Hannah Krager",
-    author_email="root.main@gmail.com",
-    license="MIT",
-    py_modules=["learning-journal"],
-    package_dir={"": "src"},
-    install_requires=['future'],
-    extras_require={
-        'test': ['pytest', 'tox']
-    },
-)
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.txt')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.txt')) as f:
+    CHANGES = f.read()
+
+requires = [
+    'pyramid',
+    'pyramid_chameleon',
+    'pyramid_debugtoolbar',
+    'pyramid_tm',
+    'SQLAlchemy',
+    'transaction',
+    'zope.sqlalchemy',
+    'waitress',
+    
+    'future',
+    'psycopg2',
+    ]
+
+tests_require = ['pytest', 'tox']
+dev_requires = ['ipython', 'pyramid-ipython']
+
+setup(name='learning-journal',
+      version='0.0',
+      description='CodeFellows 401 Learning journal',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='Kent Ross, Hannah Krager',
+      author_email='root.main@gmail.com',
+      url='',
+      keywords='web wsgi bfg pylons pyramid',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='learning-journal',
+      install_requires=requires,
+      extras_require={
+        'test': tests_require,
+        'dev': dev_requires,
+      },
+      entry_points="""\
+      [paste.app_factory]
+      main = learning_journal:main
+      [console_scripts]
+      initialize_testapp_db = testapp.scripts.initializedb:main
+      """,
+      )
