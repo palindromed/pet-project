88bb6c9fe06b9f874e7bdf25dfc7c59776c574f9
Hannah Krager <hannahkrager@gmail.com>
2016-03-15 17:12:45 -0700

    Add model tests, pshell extension

---
+++ learning_journal/tests/conftest.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+import pytest
+from sqlalchemy import create_engine
+
+from ..models import DBSession, Base
+
+
+TEST_DATABASE_URL = 'sqlite:////tmp/test_db.sqlite'
+
+
+@pytest.fixture(scope='session')
+def sqlengine(request):
+    engine = create_engine(TEST_DATABASE_URL)
+    DBSession.configure(bind=engine)
+    Base.metadata.create_all(engine)
+
+    def teardown():
+        Base.metadata.drop_all(engine)
+
+    request.addfinalizer(teardown)
+    return engine
+
+@pytest.fixture()
+def dbtransaction(request, sqlengine):
+    connection = sqlengine.connect()
+    transaction = connection.begin()
+    DBSession.configure(bind=connection)
+
+    def teardown():
+        transaction.rollback()
+        connection.close()
+        DBSession.remove()
+
+    request.addfinalizer(teardown)
+
+    return connection
+
+def test_models(dbtransaction):
+    from ..models import Post
+
+    new_post = Post(title="title", text='text')
+    assert new_post.id is None
+    assert new_post.created is None
+    DBSession.add(new_post)
+    DBSession.flush()
+    assert new_post.id is not None
+    assert new_post.created is not None
