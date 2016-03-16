fe3b38aad72a82542a273dbbfadced0dddcac2b0
Kent Ross <root.main@gmail.com>
2016-03-16 16:55:16 -0700

    changed models to use Unicode class

--- learning_journal/tests/test_models.py
+++ learning_journal/tests/test_models.py
@@ -3,7 +3,7 @@ from learning_journal.models import DBSession, Post
 
 
 def test_models(dbtransaction):
-    new_post = Post(title="title", text='text')
+    new_post = Post(title="\u1555( \u141b )\u1557", text='( \u0361\xb0 \u035c\u0296 \u0361\xb0)')
     assert new_post.id is None
     assert new_post.created is None
     DBSession.add(new_post)
