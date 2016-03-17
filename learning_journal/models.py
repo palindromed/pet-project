3cab5b33325285903c874bdc9b91dabb9b78c0d1
Hannah Krager <hannahkrager@gmail.com>
2016-03-16 18:07:56 -0700

    start work on templates

--- learning_journal/models.py
+++ learning_journal/models.py
@@ -2,9 +2,8 @@ import datetime
 from sqlalchemy import (
     Column,
     Integer,
-    Text,
+    Unicode,
     DateTime,
-    String
     )
 
 from sqlalchemy.ext.declarative import declarative_base
@@ -23,6 +22,6 @@ Base = declarative_base()
 class Post(Base):
     __tablename__ = "posts"
     id = Column(Integer, primary_key=True)
-    title = Column(String(length=128, convert_unicode=True), unique=True)
-    text = Column(Text(convert_unicode=True))
+    title = Column(Unicode(length=128), unique=True)
+    text = Column(Unicode)
     created = Column(DateTime, default=datetime.datetime.utcnow)
