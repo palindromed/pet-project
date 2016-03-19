d6a4f27059b29a821a593b1557e472ab9e4bf610
Kent Ross <root.main@gmail.com>
2016-03-19 16:27:28 -0700

    coalesce null in filters

--- learning_journal/jinja_filters.py
+++ learning_journal/jinja_filters.py
@@ -1,9 +1,22 @@
 # coding=utf-8
+from __future__ import unicode_literals
+
+from markdown import markdown as markdown_
 
 
 def dateformat(date):
+    if not date:
+        return ""
     return date.strftime('%Y-%m-%d')
 
 
 def datetimeformat(date):
+    if not date:
+        return ""
     return date.strftime('%Y-%m-%d %I:%M %p')
+
+
+def markdown(text):
+    if not text:
+        return ""
+    return markdown_(text)
