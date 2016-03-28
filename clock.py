d8f12b7a0eb0e4457fd7cd21829b7b70dc85a4f4
Hannah Krager <hannahkrager@gmail.com>
2016-03-28 12:12:16 -0700

    try using requests with sched

--- clock.py
+++ clock.py
@@ -1,11 +1,13 @@
 from apscheduler.schedulers.blocking import BlockingScheduler
-
+import requests
 sched = BlockingScheduler()
 
 
 @sched.scheduled_job('interval', minutes=2)
 def timed_job():
+    response = requests.get('http://www.google.com')
     print('This should print every 2 minutes')
+    print(response.status_code)
 
 
 # @sched.scheduled_job('cron', day_of_week='mon', hour=13)
