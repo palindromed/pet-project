152536db68b8227e1fbfb6833d5827acc0541cb9
Hannah Krager <hannahkrager@gmail.com>
2016-03-28 12:03:27 -0700

    tweak code. Not starting clock

--- clock.py
+++ clock.py
@@ -3,14 +3,14 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('interval', minutes=5)
+@sched.scheduled_job('interval', minutes=2)
 def timed_job():
-    print('This should print every 5 minutes')
+    print('This should print every 2 minutes')
 
 
-@sched.scheduled_job('cron', day_of_week='mon', hour=13)
-def scheduled_job():
-    print('I should print at 1pm')
+# @sched.scheduled_job('cron', day_of_week='mon', hour=13)
+# def scheduled_job():
+#     print('I should print at 1pm')
 
 
 sched.start()
